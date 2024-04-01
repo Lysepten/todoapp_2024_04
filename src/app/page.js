3ae973310102eb9efc8e84381a953d520be281bf
Lysepten <tlstlsrbrb11@gmail.com>
2024-04-01 16:42:18 +0900

    button에 tailwind와 mui 적용

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
+'use client';
 import Image from 'next/image';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        안녕~~~
-        <div></div>
-      </div>
-    </main>
+    <>
+      <Button className="bg-red-500" variant="contained">
+        Contained
+      </Button>
+    </>
   );
 }
